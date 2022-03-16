46b78fad6ba744db0dd218f12c4bbd3d8da4910d
An-nay-marks <87384853+An-nay-marks@users.noreply.github.com>
2022-03-16 17:36:22 +0100

    restructured data folder to hold more than one dataset and added requirements and utils

---
+++ utils.py
@@ -0,0 +1,8 @@
+'''
+This file absolutely has to be in the root directory of the project, because of the ROOT_DIR Value
+DO NOT MOVE!
+'''
+
+import os
+# global constants
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
