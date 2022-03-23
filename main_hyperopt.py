017fb1a3fd4ee560a2b44d631fe527d6c392a2fc
An-nay-marks <87384853+An-nay-marks@users.noreply.github.com>
2022-03-23 18:21:34 +0100

    help documentation

--- main_hyperopt.py
+++ main_hyperopt.py
@@ -10,8 +10,8 @@ from hyperopt_.param_spaces_UNet import *
 filter_args = ['h', 'search_space', 's', 'num_runs', 'n']
 
 parser = argparse.ArgumentParser(description='Implementation of Hyperparameter Optimizer that searches through the parameter space')
-parser.add_argument('-s', '--search_space', type=str)
-parser.add_argument('-n', '--num_runs', type = int)
+parser.add_argument('-s', '--search_space', type=str, help="Give the name of the search space, which has to be defined in 'hyperopt\\param_space_...'")
+parser.add_argument('-n', '--num_runs', type = int, help="The number of Hyperopt runs, aka models trained with different parameters from the search space")
 params = parser.parse_args()
 
 optimizer = HyperParamOptimizer(eval(params.search_space))
