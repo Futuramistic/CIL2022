ecdc1dc5a4619f5f8eb990d7ce39e282b224acd2
sekaiwodamase <ngueddach@student.ethz.ch>
2022-04-29 20:44:14 +0200

    Simplify torch prediction model parametrization

--- torch_predictor.py
+++ torch_predictor.py
@@ -1,6 +1,8 @@
 # Imports
 import torch
 from tqdm import tqdm
+
+from factory import Factory
 from models.learning_aerial_image_segmenation_from_online_maps.Unet import UNet
 from data_handling.dataloader_torch import TorchDataLoader
 from trainers.u_net import UNetTrainer
@@ -11,22 +13,26 @@ from utils import *
 # Fixed constants
 offset = 144  # Numbering of first test image
 dataset = 'original'
-
-# Parameters
-model = UNet()
-trained_model_path = 'cp_7.pt'  # Name of the pretrained model
 segmentation_threshold = 0.5
 
-# Load the model/data
-data = torch.load(trained_model_path)
-model.load_state_dict(data['model'])
+# Parameters
+model_name = 'unet'                                             # <<<<<<<<<<<<<<<<<< Insert model type
+trained_model_path = 'cp_7.pt'                                  # <<<<<<<<<<<<<<<<<< Insert trained model name
+
+# Create loader, trainer etc. from factory
+factory = Factory.get_factory(model_name)
+dataloader = factory.get_dataloader_class()(dataset=dataset)
+model = factory.get_model_class()()
+trainer = factory.get_trainer_class()(dataloader=dataloader, model=model)
+
+# Load the trained model weights
+model_data = torch.load(trained_model_path)
+model.load_state_dict(model_data['model'])
 model.eval()
 
-dataloader = TorchDataLoader(dataset=dataset)
-trainer = UNetTrainer(dataloader, model)
 preprocessing = trainer.preprocessing
-
 test_loader = dataloader.get_unlabeled_testing_dataloader(batch_size=1, preprocessing=preprocessing)
+
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model = model.to(device)
 
