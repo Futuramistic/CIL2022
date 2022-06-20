bb1af5438a3fef64e581e7ce2eb57c51b1dec67c
sekaiwodamase <ngueddach@student.ethz.ch>
2022-06-20 19:48:19 +0200

    Add another hyperopt unet parameter space (mainly for testing)

--- hyperopt_/param_spaces_UNet.py
+++ hyperopt_/param_spaces_UNet.py
@@ -28,3 +28,44 @@ unet_1 = {
         }
     }
 }
+
+unet_2 = {
+    'model': {
+        'model_type': 'unettf',  # string, to search for the corresponding factory using factory.py
+        'saving_directory': f"{ROOT_DIR}/archive/trials/UNetTF_07_06_2022_0",
+        # use kwargs for class-specific parameters, as hyperopt is written generically
+        'kwargs': {
+            'n_channels': 3,
+            'n_classes': 1,
+            'dropout': 0,
+            'kernel_init': None,
+            'kernel_regularizer': None,
+            'use_learnable_pool': True,
+            'up_transpose': hp.choice('up_transpose', [True, False]),
+        },
+    },
+    'dataset': {
+        'name': 'original',
+    },
+    'training': {
+        'minimize_loss': True, # always specify, as hyperopt can only minimize losses and therefore adapts the sign
+        'trainer_params':{
+            # 'preprocessing': ...,  <-- currently not needed I guess
+            'experiment_name': 'mlflow_test_exp', # optional
+            'split': 0.9,
+            'num_epochs': 80,
+            'batch_size': scope.int(hp.quniform('batch_size', low=2, high=8, q=2)),
+            'loss_function': 'FocalLoss',
+            'num_samples_to_visualize': 16,
+            'checkpoint_interval': 500,
+            'loss_function_hyperparams': {
+                'alpha': hp.quniform('alpha', low=0.1, high=0.9, q=0.2),
+                'gamma': hp.quniform('gamma', low=1, high=5, q=1),
+            },
+        },
+        # 'kwargs': {
+        #     'loss_alpha': hp.quniform('loss_alpha', low=0.1, high=0.9, q=0.2),
+        #     'loss_gamma': hp.quniform('loss_gamma', low=1, high=5, q=1),
+        # }
+    }
+}
