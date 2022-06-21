d7efdaca8f9a9297cbb3a38d7b2a08f20de3e6d2
Mateusz Nowak <mateusz.michal.nowak98@gmail.com>
2022-06-21 15:01:37 +0200

    Fix loss function

--- losses/u2net_loss.py
+++ losses/u2net_loss.py
@@ -1,13 +1,14 @@
 import tensorflow as tf
 import keras.backend as K
 
-def U2NET_BCE(y_true, y_pred):
+def U2NET_BCE(targets, inputs):
+    targets = K.flatten(tf.cast(targets,tf.float32))
     y_pred = tf.expand_dims(y_pred, axis=-1)
-    loss0 = K.binary_crossentropy(y_true, y_pred[0])
-    loss1 = K.binary_crossentropy(y_true, y_pred[1])
-    loss2 = K.binary_crossentropy(y_true, y_pred[2])
-    loss3 = K.binary_crossentropy(y_true, y_pred[3])
-    loss4 = K.binary_crossentropy(y_true, y_pred[4])
-    loss5 = K.binary_crossentropy(y_true, y_pred[5])
-    loss6 = K.binary_crossentropy(y_true, y_pred[6])
+    loss0 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[0],tf.float32)))
+    loss1 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[1],tf.float32)))
+    loss2 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[2],tf.float32)))
+    loss3 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[3],tf.float32)))
+    loss4 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[4],tf.float32)))
+    loss5 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[5],tf.float32)))
+    loss6 = K.binary_crossentropy(targets, K.flatten(tf.cast(inputs[6],tf.float32)))
     return loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
