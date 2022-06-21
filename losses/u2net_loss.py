be3cdabdacbeee9415fd2fddb73c4b01e892b07a
Mateusz Nowak <mateusz.michal.nowak98@gmail.com>
2022-06-21 14:51:43 +0200

    Fix loss function

--- losses/u2net_loss.py
+++ losses/u2net_loss.py
@@ -3,11 +3,11 @@ import keras.backend as K
 
 def U2NET_BCE(y_true, y_pred):
     y_pred = tf.expand_dims(y_pred, axis=-1)
-    loss0 = K.BinaryCrossentropy(y_true, y_pred[0])
-    loss1 = K.BinaryCrossentropy(y_true, y_pred[1])
-    loss2 = K.BinaryCrossentropy(y_true, y_pred[2])
-    loss3 = K.BinaryCrossentropy(y_true, y_pred[3])
-    loss4 = K.BinaryCrossentropy(y_true, y_pred[4])
-    loss5 = K.BinaryCrossentropy(y_true, y_pred[5])
-    loss6 = K.BinaryCrossentropy(y_true, y_pred[6])
+    loss0 = K.binary_crossentropy(y_true, y_pred[0])
+    loss1 = K.binary_crossentropy(y_true, y_pred[1])
+    loss2 = K.binary_crossentropy(y_true, y_pred[2])
+    loss3 = K.binary_crossentropy(y_true, y_pred[3])
+    loss4 = K.binary_crossentropy(y_true, y_pred[4])
+    loss5 = K.binary_crossentropy(y_true, y_pred[5])
+    loss6 = K.binary_crossentropy(y_true, y_pred[6])
     return loss0 + loss1 + loss2 + loss3 + loss4 + loss5 + loss6
