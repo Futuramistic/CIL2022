a1feae00a8570e78e1f82d922581d87eb4e91880
Mateusz Nowak <mateusz.michal.nowak98@gmail.com>
2022-06-23 15:54:12 +0200

    Add 0 padding to be UNet friendly

--- models/TF/blocks.py
+++ models/TF/blocks.py
@@ -32,7 +32,7 @@ class ConvoRelu_Block(tf.keras.layers.Layer):
         self.normalize = normalize  
         self.convo = Conv2D(filters=filters, kernel_size=kernel_size, padding='same', kernel_initializer=kernel_init,
                             name=name+"-conv2D", kernel_regularizer=kernel_regularizer,dilation_rate=dilation_rate)
-        self.norm = BatchNormalization(name=name+"-batchNorm")
+        self.norm = BatchNormalization(name=name+"-batchNorm", axis=3)
         self.actv = LeakyReLU()
         self.drop = Dropout(rate=dropout,name=name+"-drop")
     
