Fix rotation. Add shuffling. Fix f1 reporting for U^2Net?

data_handling/dataloader_tf.py
@@ -124,7 +124,7 @@ class TFDataLoader(DataLoader):
         print(f'Test data consists of ({test_size}) samples')
 
         if self.use_augmentation:
-            return self.training_data.map(self.augmentation,tf.data.AUTOTUNE).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+            return self.shuffle(1000).training_data.map(self.augmentation,tf.data.AUTOTUNE).batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
         return self.training_data.batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
@@ -213,8 +213,11 @@ class TFDataLoader(DataLoader):
         rotation = tf.keras.Sequential([
             tf.keras.layers.RandomRotation(0.1,seed=seed,fill_mode='constant'),
         ])
-        image = rotation(image)
-        label = rotation(label)
+        image = tf.cast(tf.expand_dims(image, 0), tf.float32)
+        label = tf.cast(tf.expand_dims(label, 0), tf.float32)
+
+        image = rotation(image)[0]
+        label = rotation(label)[0]
 
 
         return image, label

trainers/trainer_tf.py
@@ -237,9 +237,8 @@ class TFTrainer(Trainer, abc.ABC):
             output = self.model(x)
             
             # More channels than needed - U^2-Net-style
-            # Collapse into 3 channels - one image
             if(len(output.shape)==5):
-                output = output[0][0]
+                output = output[0]
                 
             preds = tf.cast(output >= self.segmentation_threshold, tf.dtypes.int8)
             precision, recall, f1_score = precision_recall_f1_score_tf(preds, y)