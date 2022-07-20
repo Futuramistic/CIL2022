6d861b27d9a9d74fdc6c400fe4c86f762a129654
Alexey Gavryushin <a.gavryushin@tum.de>
2022-07-20 22:15:50 +0000

    Add Hyperopt param space for CRANet

---
+++ hyperopt_/param_spaces_cranet.py
@@ -0,0 +1,36 @@
+from hyperopt import hp
+from hyperopt.pyll.base import scope
+import numpy as np
+from utils import ROOT_DIR
+
+
+param_space_cranet = {
+    'model': {
+        'model_type': 'cranet',
+        'saving_directory': f"{ROOT_DIR}/archive/models/cranet",
+        # use kwargs for class-specific parameters, as hyperopt is written generically
+        'kwargs': { }
+    },
+    'dataset': {
+        'name': 'original_split_1',
+        'dataloader_params': {
+            'use_geometric_augmentation': True,
+            'use_color_augmentation': True
+        }
+    },
+    'training': {
+        'minimize_loss': True,  # always specify, as hyperopt can only minimize losses and therefore adapts the sign
+        'trainer_params': {
+            'experiment_name': 'Baseline evaluation',
+            'split': 0.827,
+            'num_epochs': 300,
+            'batch_size': hp.logquniform('batch_size', low=2, high=16, q=2),
+            'checkpoint_interval': 250,
+            'hyper_seg_threshold': True
+
+        },
+        'optimizer_params': {
+            'optimizer_lr': hp.loguniform('learning_rate', np.log(5e-5), np.log(1e-2))
+        }
+    }
+}
