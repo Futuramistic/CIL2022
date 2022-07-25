45971ac7db084b202d931f57b14518790c1a711b
Mateusz Nowak <mateusz.michal.nowak98@gmail.com>
2022-07-25 16:32:50 +0200

    Revert reduce retracing - unsupported?

--- data_handling/dataloader_tf.py
+++ data_handling/dataloader_tf.py
@@ -280,7 +280,7 @@ class TFDataLoader(DataLoader):
         """
         tf.keras.models.save_model(model, filepath)
 
-    @tf.function(reduce_retracing=True)
+    @tf.function
     def augmentation(self, image, label):
         """
         Apply various data augmentations to the image
