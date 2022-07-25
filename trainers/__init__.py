e083d14728a9b0dff03ccd173edd1946a726f306
Noureddine Gueddach <noureddach@gmail.com>
2022-07-25 17:15:42 +0200

    Rename trainers

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -2,14 +2,14 @@ from .trainer import Trainer
 from .trainer_tf import TFTrainer
 from .trainer_torch import TorchTrainer
 
-from .AttUnetPlusPlusTF import AttUNetPlusPlusTrainer
-from .AttUnetTF import AttUNetTrainer
-from .gl_dense_u_net import GLDenseUNetTrainer
-from .u_net import UNetTrainer
-from .UnetPlusPlusTF import UNetPlusPlusTrainer
-from .UnetTF import UNetTFTrainer
-from .cra_net import CRANetTrainer
-from .deep_lab_v3 import DeepLabV3Trainer
-from .deeplabv3gan import DeepLabV3PlusGANTrainer
+from .attunetplusplus_tf_trainer import AttUNetPlusPlusTrainer
+from .attunet_tf_trainer import AttUNetTrainer
+from .gldenseunet_tf_trainer import GLDenseUNetTrainer
+from .unet_torch_trainer import UNetTrainer
+from .unetplusplus_tf_trainer import UNetPlusPlusTrainer
+from .unet_tf_trainer import UNetTFTrainer
+from .cranet_torch_trainer import CRANetTrainer
+from .deeplabv3_torch_trainer import DeepLabV3Trainer
+from .deeplabv3gan_torch_trainer import DeepLabV3PlusGANTrainer
 from .rl_trainer_torch import TorchRLTrainer
-from .segformer import SegFormerTrainer
+from .segformer_torch_trainer import SegFormerTrainer
