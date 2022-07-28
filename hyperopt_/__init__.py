79edbbcb1605d27e8155772cdc0c5e0df6dfc7e7
An-nay-marks <87384853+An-nay-marks@users.noreply.github.com>
2022-07-28 16:11:07 +0200

    fix hyperopt init

--- hyperopt_/__init__.py
+++ hyperopt_/__init__.py
@@ -6,3 +6,4 @@ from .param_spaces_cranet import *
 from .param_spaces_deeplabv3 import *
 from .param_spaces_segformer import *
 from .param_spaces_RL import *
+from .param_spaces_AttUnetTF import *
