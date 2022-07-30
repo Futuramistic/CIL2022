975079f3b79880c9b986c50734ec13f773493691
An-nay-marks <87384853+An-nay-marks@users.noreply.github.com>
2022-07-30 22:10:00 +0200

    refactor main/comment/help

--- main.py
+++ main.py
@@ -25,9 +25,7 @@ import torch.nn
 import torch.optim
 import torch.nn.functional as F
 
-
-
-def main():
+def parse_args():
     # all args that cannot be matched to the Trainer or DataLoader classes and are
     # not in filter_args will be passed to the
     # model's constructor
@@ -47,34 +45,58 @@ def main():
     filter_args = ['h', 'model', 'm', 'evaluate', 'eval', 'V', 'seed', 'S', 'adaboost_runs', 'A']
 
     parser = argparse.ArgumentParser(description='Implementation of ETHZ CIL Road Segmentation 2022 project')
-    parser.add_argument('-m', '--model', type=str, required=True)
-    parser.add_argument('-S', '--seed', type=int, default=1, required=False)
-    parser.add_argument('-E', '--experiment_name', type=str, required=False)
-    parser.add_argument('-R', '--run_name', type=str, required=False)
-    parser.add_argument('-s', '--split', type=float, default=DEFAULT_TRAIN_FRACTION, required=False)
-    parser.add_argument('-e', '--num_epochs', type=int, required=False, default=2)
-    parser.add_argument('-b', '--batch_size', type=int, required=False)
-    parser.add_argument('-L', '--loss_function', type=str, required=False)
+    parser.add_argument('-m', '--model', type=str, required=True, 
+                        help="The (abbreviated) name of the model to train.")
+    parser.add_argument('-d', '--dataset', type=str, required=True, 
+                        help="The name of the dataset. If not available locally, the dataset will be downloaded.")
+    parser.add_argument('-S', '--seed', type=int, required=False, default=1, 
+                        help="The random seed. Defaults to 1.")
+    parser.add_argument('-E', '--experiment_name', type=str, required=False, 
+                        help="The experiment name, which will also be used to log to MLFlow. Default to the model name \
+                        with an additional integer value of the current time.")
+    parser.add_argument('-R', '--run_name', type=str, required=False, 
+                        help="The run name, which can be used for logging, in order to distinguish runs of the same \
+                        experiment.")
+    parser.add_argument('-s', '--split', type=float, required=False, default=DEFAULT_TRAIN_FRACTION, 
+                        help="The percentile of the training data used for actual training. The rest will be used for \
+                        evaluation.")
+    parser.add_argument('-e', '--num_epochs', type=int, required=False, default=10, 
+                        help="The number of epochs to train the model on. Defaults to 10.")
+    parser.add_argument('-b', '--batch_size', type=int, required=False, default=4, 
+                        help="The batch size. Defaults to 4.")
+    parser.add_argument('-L', '--loss_function', type=str, required=False, 
+                        help="The name of the loss function to be used. Each trainer has its own default loss function.")
     # json.loads: substitute for dict
-    parser.add_argument('-H', '--loss_function_hyperparams', type=json.loads, required=False, default=None)
-    parser.add_argument('-i', '--evaluation_interval', type=float, required=False, default=10)
-    parser.add_argument('-v', '--num_samples_to_visualize', type=int, required=False)
-    parser.add_argument('-c', '--checkpoint_interval', type=int, required=False)
-    parser.add_argument('-C', '--load_checkpoint_path', '--from_checkpoint', type=str, required=False)
-    parser.add_argument('-t', '--segmentation_threshold', type=float, default=DEFAULT_SEGMENTATION_THRESHOLD,
-                        required=False)
-    parser.add_argument('-f', '--f1_threshold_to_log_checkpoint', type=float, default=DEFAULT_F1_THRESHOLD_TO_LOG_CHECKPOINT,
-                        required=False)
-    parser.add_argument('-B', '--blobs_removal_threshold', type=int, default=DEFAULT_BLOBS_REMOVAL_THRESHOLD,
-                        required=False)
-    parser.add_argument('-d', '--dataset', type=str, required=True)
-    parser.add_argument('-V', '--evaluate', '--eval', action='store_true')
-    parser.add_argument('-T', '--hyper_seg_threshold', type=bool,
-                        help="If True, use hyperparameter search after evaluation to find the best "
-                             "segmentation threshold", required=False, default=True)
+    parser.add_argument('-H', '--loss_function_hyperparams', type=json.loads, required=False, default=None, 
+                        help="A json loaded dictionary to specify the hyperparameters for the loss function. Every loss \
+                        function has it's own default hyperparameters.")
+    parser.add_argument('-i', '--evaluation_interval', type=float, required=False, default=10, 
+                        help="The interval in iterations in which to perform evaluation. Defaults to 10.")
+    parser.add_argument('-v', '--num_samples_to_visualize', type=int, required=False, 
+                        help="The number of samples to be logged to mlflow during the evaluation.")
+    parser.add_argument('-c', '--checkpoint_interval', type=int, required=False, 
+                        help="The interval in iterations in which to log the model's checkpoint locally or to MLFlow. \
+                        The checkpoint is only saved if the model has reached a certain f1 score specified by the \
+                        'f1_threshold_to_log_checkpoint', in order to save time.")
+    parser.add_argument('-C', '--load_checkpoint_path', '--from_checkpoint', type=str, required=False, 
+                        help="The local checkpoint path or mlflow checkpoint if a training should be resumed or the testing \
+                        should be performed with the specified model parameters.")
+    parser.add_argument('-t', '--segmentation_threshold', type=float, required=False, default=DEFAULT_SEGMENTATION_THRESHOLD,
+                        help="Given a models float prediction, the segmentation thresholds creates a binary output, where 1 is\
+                        where the model's prediction is bigger than this threshold and 0 otherwise.")
+    parser.add_argument('-f', '--f1_threshold_to_log_checkpoint', type=float, required=False, 
+                        default=DEFAULT_F1_THRESHOLD_TO_LOG_CHECKPOINT, 
+                        help="The minimum f1 validation score that is necessary to log the model. Worse models will not be \
+                        logged for time efficiency reasons.")
+    parser.add_argument('-B', '--blobs_removal_threshold', type=int, required=False, default=DEFAULT_BLOBS_REMOVAL_THRESHOLD,
+                        help="Some models tend to create false positive 'blobs'. Use this threshold to remove these blobs.")
+    parser.add_argument('-V', '--evaluate', '--eval', action='store_true', 
+                        help="Use this flag if you want to test an existing model or a new model after training.")
+    parser.add_argument('-T', '--hyper_seg_threshold', type=bool, required=False, default=True,
+                        help="If True, use hyperparameter search after evaluation to find the best segmentation threshold.")
     parser.add_argument('-w', '--use_sample_weighting', type=bool, required=False, default=False,
-                        help="If True, use sample weighting during training to train more on samples with big errors.\
-                            Currently only working with torch")
+                        help="If True, use sample weighting during training to train more on samples with big errors. The sample \
+                        weights are adapted after each epoch. Currently only working with Torch models and disabled in TF")
     parser.add_argument('-a', '--use_adaboost', type=bool, required=False, default=False,
                         help="If True, apply the Adaboost algorithm to the training (original or deep version)")
     parser.add_argument('-D', '--deep_adaboost', type=bool, required=False, default=False,
@@ -121,6 +143,10 @@ def main():
     trainer_spec_args = {k: v for k, v in arg_dict.items() if k.lower() in trainer_args}
     dataloader_spec_args = {k: v for k, v in arg_dict.items() if k.lower() in dataloader_args}
     
+    return factory, model_spec_args, trainer_spec_args, dataloader_spec_args, known_args_dict, unknown_args_dict, known_args
+
+
+def main(factory, model_spec_args, trainer_spec_args, dataloader_spec_args, known_args_dict, unknown_args_dict, known_args):
     # call adaboost script if adaboost is used
     if known_args_dict["use_adaboost"]:
         adabooster = AdaBooster(factory, known_args_dict, unknown_args_dict, model_spec_args, trainer_spec_args,
@@ -177,9 +203,12 @@ if __name__ == '__main__':
     for path in [stderr_path, stdout_path]:
         if os.path.isfile(path):
             os.unlink(path)
-
+    
+    # parse arguments before redirecting the logs, in order to be able to print the --help comments
+    args = parse_args()
+    
     if IS_DEBUG:
-        main()
+        main(*args)
     else:  # If not running in debug mode, redirect the stdout and stderr to some log files
         try:
             print(f'Session ID: {SESSION_ID}\n'
@@ -188,7 +217,7 @@ if __name__ == '__main__':
             # buffering=1: use line-by-line buffering
             with open(stderr_path, 'w', buffering=1) as stderr_f, open(stdout_path, 'w', buffering=1) as stdout_f:
                 with redirect_stderr(stderr_f), redirect_stdout(stdout_f):
-                    main()
+                    main(*args)
         except Exception as e:
             err_msg = f'*** Exception encountered: ***\n{e}'
             pushbullet_logger.send_pushbullet_message(err_msg)
