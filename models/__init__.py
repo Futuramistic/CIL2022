0e6d56f2342b0f08917c8fcd76c87ecc05d54ff3
Mateusz Nowak <mateusz.michal.nowak98@gmail.com>
2022-07-31 16:50:29 +0200

    Remove ghost models from init

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,10 @@
 from .TF.gl_dense_u_net import GLDenseUNet
-from .Torch.Unet import UNet
-from .Torch.Deeplabv3 import *
 from .TF.UNetTF import UNetTF
 from .TF.AttUNetPlusPlusTF import AttUNetPlusPlusTF
 from .TF.UNetPlusPlusTF import UNetPlusPlusTF
-from .Torch.CRA_Net import OurDinkNet50
-from .Torch.DeepLabV3PlusGAN import DeepLabV3PlusGAN
 from .reinforcement.first_try import SimpleRLCNN
 from .reinforcement.first_try import SimpleRLCNNMinimal
 from .reinforcement.first_try import SimpleRLCNNMinimalSupervised
 from .reinforcement.first_try import ResNetBasedRegressor
-from .Torch.segformer import SegFormer
 from .TF.UNetExpTF import UNet3PlusTF, UNetExpTF
-from .Torch.lawin import Lawin
-from .Torch.TwoShotNet import TwoShotNet
 from .TF.U2NetTF import *
